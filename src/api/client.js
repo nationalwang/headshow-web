8e1a8b518cc8ed2410e78980019897951b954e6a
linlangleo <1529761321@qq.com>
2018-09-25 09:00:25 +0800

    接口地址修改

--- src/api/client.js
+++ src/api/client.js
@@ -9,7 +9,7 @@ class ApiClient {
   generateApiURI (uri) {
     // return 'http://119.23.18.224:168' + uri // 服务器
     // return 'http://203.195.203.71:168' + uri // 服务器
-    return 'http://193.112.103.154.71:168' + uri // 服务器
+    return 'http://193.112.103.154:168' + uri // 服务器
     // return 'http://localhost:168' + uri // 本地
   }
 
