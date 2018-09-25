2ceb684b83c10af8a5fbb013ce1af7876e8ca09a
linlangleo <1529761321@qq.com>
2018-09-25 08:20:37 +0800

    接口地址修改

--- src/api/client.js
+++ src/api/client.js
@@ -7,7 +7,9 @@ Vue.use(VueAxios, axios)
 
 class ApiClient {
   generateApiURI (uri) {
-    return 'http://119.23.18.224:168' + uri // 服务器
+    // return 'http://119.23.18.224:168' + uri // 服务器
+    // return 'http://203.195.203.71:168' + uri // 服务器
+    return 'http://193.112.103.154.71:168' + uri // 服务器
     // return 'http://localhost:168' + uri // 本地
   }
 
